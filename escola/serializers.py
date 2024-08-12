c65097a484738f13f244292974664293d23e8753
marcelodiass <marcelodias.desenvolvedor@gmail.com>
2024-08-12 11:57:39 -0300

    Serializers matricula + estudantes + curso

--- escola/serializers.py
+++ escola/serializers.py
@@ -18,4 +18,19 @@ class MatriculaSerializer(serializers.ModelSerializer):
         model = Matricula
         exclude = []
         
-        
+class ListaMatriculasEstudanteSerializer(serializers.ModelSerializer):
+    curso = serializers.ReadOnlyField(source='curso.descricao')
+    periodo = serializers.SerializerMethodField()
+    class Meta:
+        model = Matricula
+        fields = ['curso', 'periodo']
+    
+    def get_periodo(self, obj):
+        return obj.get_periodo_display()
+    
+
+class ListaMatriculasCursoSerializer(serializers.ModelSerializer):
+    estudante_nome = serializers.ReadOnlyField(source='estudante.nome')
+    class Meta:
+        model = Matricula
+        fields = ['estudante_nome']
