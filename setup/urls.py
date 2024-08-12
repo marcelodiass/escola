4a0a6dcbd2704f21d3f5ed418c1511935f184f2d
marcelodiass <marcelodias.desenvolvedor@gmail.com>
2024-08-12 10:58:03 -0300

    definindo as urls com router

--- setup/urls.py
+++ setup/urls.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
-from django.urls import path
-from escola.views import estudantes
+from django.urls import path, include
+from escola.views import EstudanteViewSet, CursoViewSet
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register('estudantes', EstudanteViewSet, basename='Estudantes')
+router.register('cursos', CursoViewSet, basename='Cursos')
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('estudantes/', estudantes)
+    path('', include(router.urls))
 ]
