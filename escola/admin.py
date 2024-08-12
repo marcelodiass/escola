984f8f6bc61cdd462b3585a51d71be8e6e3144fa
marcelodiass <marcelodias.desenvolvedor@gmail.com>
2024-08-12 09:45:14 -0300

    configurando admin

--- escola/admin.py
+++ escola/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from escola.models import Estudante, Curso
 
-# Register your models here.
+class Estudantes(admin.ModelAdmin):
+    list_display = ('id','nome','email','data_nascimento','celular',)
+    list_display_links = ('id','nome',)
+    list_per_page = 20
+    search_fields = ('nome',)
+    
+admin.site.register(Estudante,Estudantes)
+
+class Cursos(admin.ModelAdmin):
+    list_display = ('id','codigo','descricao',)
+    list_display_links = ('id','codigo',)
+    search_fields = ('codigo',)
+
+admin.site.register(Curso, Cursos)
