9f35b6cc5c214acf3fc2a3ad8570267092f85c6a
marcelodiass <marcelodias.desenvolvedor@gmail.com>
2024-08-12 00:56:31 -0300

    Curso model

--- escola/models.py
+++ escola/models.py
@@ -10,3 +10,18 @@ class Estudante(models.Model):
     
     def __str__(self):
         return self.nome
+    
+
+class Curso(models.Model):
+    NIVEL = (
+        ('B', 'Básico'),
+        ('I', 'Intermediário'),
+        ('A', 'Avançado'),
+    )
+    
+    codigo = models.CharField(max_length=10)
+    descricao = models.CharField(max_length=100, blank=False)
+    nivel = models.CharField(max_length=1, blank=False, null=False, choices=NIVEL, default='B')
+    
+    def __str__(self):
+        return self.codigo
