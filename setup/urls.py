da751d72d9131cb550f18c4cd8f6d47cc59af1c9
marcelodiass <marcelodias.desenvolvedor@gmail.com>
2024-08-12 11:44:38 -0300

    URL com route de Matriculas

--- setup/urls.py
+++ setup/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
-from escola.views import EstudanteViewSet, CursoViewSet
+from escola.views import EstudanteViewSet, CursoViewSet, MatriculaViewSet
 from rest_framework import routers
 
 router = routers.DefaultRouter()
 router.register('estudantes', EstudanteViewSet, basename='Estudantes')
 router.register('cursos', CursoViewSet, basename='Cursos')
+router.register('matriculas', MatriculaViewSet, basename='Matriculas')
 
 
 urlpatterns = [
