97978c36d2e5126648469d66718ad846642ab0bc
marcelodiass <marcelodias.desenvolvedor@gmail.com>
2024-08-12 00:51:15 -0300

    Estudante model

--- escola/models.py
+++ escola/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Estudante(models.Model):
+    nome = models.CharField(max_length=100)
+    email = models.EmailField(blank=False)
+    cpf = models.CharField(max_length=11)
+    data_nascimento = models.DateField()
+    celular = models.CharField(max_length=14)
+    
+    def __str__(self):
+        return self.nome
