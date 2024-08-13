58118cc9f49667a0d4a35911b805b055c129db11
marcelodiass <marcelodias.desenvolvedor@gmail.com>
2024-08-13 10:57:25 -0300

    validações nos modelos

--- escola/models.py
+++ escola/models.py
@@ -1,10 +1,11 @@
 from django.db import models
+from django.core.validators import MinLengthValidator
 
 
 class Estudante(models.Model):
     nome = models.CharField(max_length=100)
     email = models.EmailField(blank=False)
-    cpf = models.CharField(max_length=11)
+    cpf = models.CharField(max_length=11, unique=True)
     data_nascimento = models.DateField()
     celular = models.CharField(max_length=14)
     
@@ -19,7 +20,7 @@ class Curso(models.Model):
         ('A', 'Avançado'),
     )
     
-    codigo = models.CharField(max_length=10)
+    codigo = models.CharField(max_length=10, unique=True, validators=[MinLengthValidator(3)])
     descricao = models.CharField(max_length=100, blank=False)
     nivel = models.CharField(max_length=1, blank=False, null=False, choices=NIVEL, default='B')
     
